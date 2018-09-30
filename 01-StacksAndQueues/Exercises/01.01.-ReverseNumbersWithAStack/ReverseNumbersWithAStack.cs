7860189d22862acdb12aa076be5597f6b378c849
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2018-09-30 20:54:53 +0100

    Add Problem 01.-ReverseNumbersWithAStack via upload
    Add Problem 01.01.-ReverseNumbersWithAStack via upload

---
+++ 01-StacksAndQueues/Exercises/01.01.-ReverseNumbersWithAStack/ReverseNumbersWithAStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._01._ReverseNumbersWithAStack
+{
+    public class ReverseNumbersWithAStack
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            Stack<int> stackOfNumbers = new Stack<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                stackOfNumbers.Push(numbers[i]);
+            }
+
+            while (stackOfNumbers.Count > 0)
+            {
+                Console.Write(stackOfNumbers.Pop() + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
